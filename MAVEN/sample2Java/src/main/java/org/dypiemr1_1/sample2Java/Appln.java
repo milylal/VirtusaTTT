33b4d56b34d567f5f082f734e4f970942fe8dd09
milylal <milylike@gmail.com>
2020-05-10 11:54:02 +0530

    Add files via upload

---
+++ MAVEN/sample2Java/src/main/java/org/dypiemr1_1/sample2Java/Appln.java
@@ -0,0 +1,15 @@
+package org.dypiemr1_1.sample2Java;
+
+import org.dypiemr1.sampleJava.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class Appln 
+{   
+    public static void main( String[] args )
+    {   double result=App.add(5, 6);
+        System.out.println( result );
+    }
+}
