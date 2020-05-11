6ff7c0224d641c3a4bd3ebc30888c5dcdee2a59e
milylal <milylike@gmail.com>
2020-05-11 07:54:02 +0530

    Add files via upload

---
+++ MAVEN/src/main/java/org/dypiemr1_2/sampleParent/App.java
@@ -0,0 +1,13 @@
+package org.dypiemr1_2.sampleParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
